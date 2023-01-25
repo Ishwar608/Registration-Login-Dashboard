4c2e7e1a5e881cc57593199696b254eb4e599bd0
Ishwar <whitehacker608@gmail.com>
2023-01-25 12:04:23 +0530

    show password

--- src/Registration/SignIn.js
+++ src/Registration/SignIn.js
@@ -11,6 +11,16 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -19,7 +29,7 @@ import axios from 'axios';
 const theme = createTheme();
 
 export default function SignIn() {
-  const [data,setData] = React.useState({
+  const [data, setData] = React.useState({
     title: "",
     firstName: "",
     lastName: "",
@@ -32,24 +42,32 @@ export default function SignIn() {
 
     event.preventDefault();
 
-    axios.post("https://real-pear-fly-kilt.cyclic.app/accounts/register",data)
-    .then(y=>{
-      if(y.status == 200 || y.status ==201){
-        toast.success("Sucessfully Resitrastion");
-      }
-    }).catch(y=>{
-      toast.error("Error");
-    })
+    axios.post("https://real-pear-fly-kilt.cyclic.app/accounts/register", data)
+      .then(y => {
+        if (y.status == 200 || y.status == 201) {
+          toast.success("Sucessfully Resitrastion");
+        }
+      }).catch(y => {
+        toast.error("Error");
+      })
 
   };
 
-  const inputHandlar = (e) =>{
-    if(e.target.type != "checkbox"){
-      setData({...data,[e.target.name] : e.target.value});
-    }else{
-      setData({...data,[e.target.name] : e.target.checked});
+  const inputHandlar = (e) => {
+    if (e.target.type != "checkbox") {
+      setData({ ...data, [e.target.name]: e.target.value });
+    } else {
+      setData({ ...data, [e.target.name]: e.target.checked });
     }
   }
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -70,16 +88,22 @@ export default function SignIn() {
             Sign in
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="text"
-              label="Title"
-              name="title"
-              autoFocus
-              onChange={inputHandlar}
-            />
+
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Title</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Title"
+                name="title"
+                onChange={inputHandlar}
+              >
+                <MenuItem value="Mr">Mr</MenuItem>
+                <MenuItem value="Miss">Miss</MenuItem>
+                <MenuItem value="Mrs">Mrs</MenuItem>
+              </Select>
+            </FormControl>
+
             <TextField
               margin="normal"
               required
@@ -115,10 +139,24 @@ export default function SignIn() {
               fullWidth
               name="password"
               label="Password"
-              type="password"
+              // type="password"
+              type={showPassword ? 'text' : 'password'}
               id="password"
               autoComplete="current-password"
               onChange={inputHandlar}
+
+              InputProps={{
+                endAdornment: <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>,
+              }}
             />
             <TextField
               margin="normal"
@@ -126,10 +164,23 @@ export default function SignIn() {
               fullWidth
               name="confirmPassword"
               label="Confirm Password"
-              type="password"
+              // type="password"
               id="password"
+              type={showPassword ? 'text' : 'password'}
               autoComplete="current-password"
               onChange={inputHandlar}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>,
+              }}
             />
             <FormControlLabel
               control={<Checkbox typeof='checkbox' name="acceptTerms" value="remember" color="primary" onClick={inputHandlar} />}
